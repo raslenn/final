d845272261c3ea68606087df9541c4475feb19a1
raslenn <raslendridi0@gmail.com>
2022-08-26 13:43:43 +0200

    7th push

--- js/mainjs2.js
+++ js/mainjs2.js
@@ -1,80 +1,78 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+var object=new Object()
+var test=false
+var myinfo={}
 
-// Show input error message
+const form = document.getElementById("form");
+const username = document.getElementById("username").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+const password2 = document.getElementById("password2").value
+object.username=username
+object.password2
+
+
+
+//show error message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
+  //small will do change the small tag in HTML
   const small = formControl.querySelector("small");
   small.innerText = message;
 }
-
-// Show success outline
+//show success message
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
-// Check email is valid
+//check for valid email
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
-    showError(input, "Email is not valid");
+    showError(input, 'Email is not valid');
   }
 }
-
-// Check required fields
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
+//check length of input
+function checkLength(input,min,max){
+    if(input.value.length<min){
+        showError(input,`${getFieldName(input)} must be at least ${min} characters`)
+    }else if(input.value.length>max){
+        showError(input,`${getFieldName(input)} must be at less than ${max} characters`)
+    }else {
+        showSuccess(input);
     }
-  });
+
+}
+//get fieldName
+function getFieldName(input){
+    return input.id;
+}
+//check required 
+function checkRequired(inputArray){
+    inputArray.map(input =>{
+        // console.log(input.value)
+        if(input.value.trim() === ''){
+            showError(input,`${input.id} is required`)
+        }else{
+            showSuccess(input);
+        }
+    })
+}
+//check for password match
+function checkPassword(input1,input2){
+    if(input1.value!==input2.value){ 
+        showError(input2,'Password did not matched');
+    }
 }
 
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
-}
-
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, "Passwords do not match");
-  }
-}
-
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-// Event listeners
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
-  checkEmail(email);
-  checkPasswordsMatch(password, password2);
-});
+   checkRequired([username,email,password,password2]);
+   checkLength(username,3,15);
+   checkLength(password,6,25);
+   checkEmail(email); 
+   checkPassword(password,password2);
